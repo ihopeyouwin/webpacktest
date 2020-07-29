9aaf7bc148c98a68dc59fbc3a53fc249a20299cc
ihopeyouwin <ihopeyouwon@gmail.com>
2020-07-29 15:48:54 +0300

    init

---
+++ src/analytics.js
@@ -0,0 +1,21 @@
+function analytics () {
+  let counter = 0
+  let isDestroyed = false
+  const listener = () => counter++
+  document.addEventListener('click', listener)
+
+  return {
+    destroy(){
+      document.removeEventListener('click', listener)
+      isDestroyed = true
+    },
+    getClicks(){
+      if(isDestroyed){
+        return 'analytics destroyed'
+      }
+      return counter
+    }
+  }
+}
+
+window.analytics = analytics();
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Post from "./Post";
+
+const post = new Post('webpack test title')
+
+console.log(post.toString())
